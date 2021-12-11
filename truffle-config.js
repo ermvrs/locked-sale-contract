dfe6d215f6bc80d0d2380b8d1cab2419c866d405
ermvrs <ermvrs@live.com>
2021-12-11 12:17:56 +0300

    init

---
+++ truffle-config.js
@@ -0,0 +1,73 @@
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+require('dotenv').config();
+
+const DEPLOYER = process.env.DEPLOYER;
+
+module.exports = {
+  networks: {
+    development: {
+      host: "127.0.0.1",     // Localhost (default: none)
+      port: 8545,            // Standard BSC port (default: none)
+      network_id: "*",       // Any network (default: none)
+    },
+    testnet: {
+      provider: () => new HDWalletProvider(DEPLOYER, `https://data-seed-prebsc-1-s1.binance.org:8545`),
+      network_id: 97,
+      confirmations: 2,
+      timeoutBlocks: 200,
+      skipDryRun: true,
+      from: '0x6Ff850ae284951787216dbc9880e6573c53Ab822',
+    },
+    bsc: {
+      provider: () => new HDWalletProvider(DEPLOYER, `https://bsc-dataseed1.defibit.io/`),
+      network_id: 56,
+      confirmations: 5,
+      timeoutBlocks: 200,
+      skipDryRun: true,
+      from: '0x6Ff850ae284951787216dbc9880e6573c53Ab822', // deployer address ytazılacak,
+      gasPrice : 5000010000
+    },
+  },
+  plugins: [
+    'truffle-plugin-verify'
+  ],
+  api_keys: {
+    // Add BSCSCAN_API_KEY in .env file to verify contracts deployed through truffle
+    etherscan: ""
+  },
+  // Set default mocha options here, use special reporters etc.
+  mocha: {
+    // timeout: 100000
+  },
+
+  // Configure your compilers
+  compilers: {
+    solc: {
+      //https://forum.openzeppelin.com/t/how-to-deploy-uniswapv2-on-ganache/3885
+      version: "0.8.10",    // Fetch exact version from solc-bin (default: truffle's version)
+      // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
+      settings: {          // See the solidity docs for advice about optimization and evmVersion
+      optimizer: {
+        enabled: true,
+        runs: 999999
+      },
+      evmVersion: "istanbul", 
+      outputSelection: {
+        "*": {
+          "": [
+            "ast"
+          ],
+          "*": [
+            "evm.bytecode.object",
+            "evm.deployedBytecode.object",
+            "abi",
+            "evm.bytecode.sourceMap",
+            "evm.deployedBytecode.sourceMap",
+            "metadata"
+          ]
+        },
+      }
+      }
+    },
+  }
+}
